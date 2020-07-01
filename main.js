d26ecb21f60c575e4bdecd14a755ac3db2e98e7e
chescos <ch3sc0s@gmail.com>
2020-07-01 10:30:35 +0200

    added notifications

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-inspectLink = function (link) {
+inspectLink = (link) => {
     console.log(link);
 
     fetch('https://api.csgoskins.gg/inspect-links', {
@@ -15,20 +15,50 @@ inspectLink = function (link) {
         .then((res) => {
             console.log(res);
 
-            if (res.success && res.needs_to_connect && res.connect_to_url) {
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    const tab = tabs[0];
-                    chrome.tabs.update(tab.id, { url: res.connect_to_url });
+            if (res.success) {
+                if (res.needs_to_connect) {
+                    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                        const tab = tabs[0];
+                        chrome.tabs.update(tab.id, { url: res.connect_to_url });
+                    });
+
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: './assets/csgo-icon.jpg',
+                        title: 'Skin Sent',
+                        message: 'The skin has been sent to the CS:GO test server, please connect.',
+                    });
+                } else {
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: './assets/csgo-icon.jpg',
+                        title: 'Skin Equipped',
+                        message: 'You have been equipped with the selected skin in the CS:GO test server.',
+                    });
+                }
+            } else {
+                chrome.notifications.create({
+                    type: 'basic',
+                    iconUrl: './assets/csgo-icon.jpg',
+                    title: 'Request Failed',
+                    message: req.message,
                 });
             }
         })
         .catch((error) => {
             console.log(error);
+
+            chrome.notifications.create({
+                type: 'basic',
+                iconUrl: './assets/csgo-icon.jpg',
+                title: 'Request Failed',
+                message: 'Something went wrong, please try again.',
+            });
         });
 };
 
 chrome.contextMenus.create({
     title: 'Test CS:GO skin in-game',
-    contexts: ["link"],
+    contexts: ['link'],
     onclick: inspectLink
 });
